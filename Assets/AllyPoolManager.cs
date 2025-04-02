559fe2ad546069366b82b8ea78e10905f2742ca1
Ohhyuntaek <61840656+Ohhyuntaek@users.noreply.github.com>
2025-04-02 22:12:30 +0900

    Refactoring: 스폰되어 있는 Ally의 List 추가
    AllyPoolManager 스크립트의 activateAllies 리스트 참고

--- Assets/AllyPoolManager.cs
+++ Assets/AllyPoolManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [System.Serializable]
 public class AllyPool
@@ -20,6 +21,9 @@ public class AllyPoolManager : MonoBehaviour
     public static AllyPoolManager Instance;
     public AllyPool[] allyPools;
 
+    // 현재 활성화 된 Ally 리스트
+    [FormerlySerializedAs("activateAillies")] public List<GameObject> activateAllies = new();
+    
     void Awake()
     {
         Instance = this;
@@ -48,6 +52,10 @@ public class AllyPoolManager : MonoBehaviour
             obj.transform.position = spawnPos;
             obj.SetActive(true);
             obj.GetComponent<Ally>().Init(spawnPos);
+            
+            // 활성화 된 Ally로 등록
+            activateAllies.Add(obj);
+            
             return obj;
         }
 
@@ -58,11 +66,26 @@ public class AllyPoolManager : MonoBehaviour
     {
         // 타일 반환
         TileManager.Instance.FreeTile(ally.transform.position);
+
+        // 활성화 된 Ally 리스트에서 제거
+        if (activateAllies.Contains(ally))
+        {
+            activateAllies.Remove(ally);
+        }
         
         // Ally 비활성화 후 풀에 다시 넣기
         ally.SetActive(false);
         
         var pool = System.Array.Find(allyPools, p => p.allyType == type);
         pool.pool.Enqueue(ally);
+    }
+    
+    public void PrintActivateAiilies()
+    {
+        // 현재 활성화 되어 있는 Ally의 리스트를 로그에 표시
+        foreach (var ally in AllyPoolManager.Instance.activateAllies)
+        {
+            Debug.Log($"활성 Ally: {ally.name} - 위치: {ally.transform.position}");
+        }
     }
 }
